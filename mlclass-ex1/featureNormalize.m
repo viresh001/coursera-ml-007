665efcf2a3484b4903abba8e1e852bb9bd778c04
Viresh <viresh001@users.noreply.github.com>
2014-10-04 17:22:01 -0700

    Update featureNormalize.m

--- mlclass-ex1/featureNormalize.m
+++ mlclass-ex1/featureNormalize.m
@@ -11,7 +11,7 @@ mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
 
 for i = 1:size(X,2)
-	mu(i) = mean(X(:,i));
+    mu(i) = mean(X(:,i));
     sigma(i) = std(X(:,i));
     X_norm(:,i) = (X(:,i) - mu(i))/sigma(i);
 end
